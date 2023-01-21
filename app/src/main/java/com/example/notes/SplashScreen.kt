12ff18b7f209d2804058d894f620d5cd9f33e776
Viraj76 <virajg.7680@gmail.com>
2023-01-21 23:44:29 +0530

    Removed Action Bar From SplashScreen

--- app/src/main/java/com/example/notes/SplashScreen.kt
+++ app/src/main/java/com/example/notes/SplashScreen.kt
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen)
-
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
             startActivity(Intent(this, MainActivity::class.java))
 
