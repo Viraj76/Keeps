78e7b1facb673cafce85d504fc3161646bbacbd1
Viraj76 <virajg.7680@gmail.com>
2023-03-06 23:26:02 +0530

    Back End Code for Authentication

--- app/src/main/java/com/example/notes/UI/Activity/SignInActivity.kt
+++ app/src/main/java/com/example/notes/UI/Activity/SignInActivity.kt
@@ -1,12 +1,26 @@
 package com.example.notes.UI.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.notes.R
+import com.example.notes.databinding.ActivitySignInBinding
+import com.example.notes.databinding.ItemBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/notes/UI/Activity/SplashScreen.kt
+++ app/src/main/java/com/example/notes/UI/Activity/SplashScreen.kt
@@ -16,7 +16,7 @@ class SplashScreen : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen)
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, SignInActivity::class.java))
 
         }, 2000)
     }
