a0998b9beced91f3e919062530015cee8056f953
Viraj76 <virajg.7680@gmail.com>
2023-03-07 09:25:20 +0530

    BackEnd Codes for SignIn And Singup

--- app/src/main/java/com/example/notes/UI/Activity/SIgnUPActivity.kt
+++ app/src/main/java/com/example/notes/UI/Activity/SIgnUPActivity.kt
@@ -1,13 +1,55 @@
 package com.example.notes.UI.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.notes.R
+import com.example.notes.databinding.ActivitySignUpactivityBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SIgnUPActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpactivityBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_upactivity)
+        binding = ActivitySignUpactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnSignUp.setOnClickListener { signedUpUser() }
+        binding.tvSignInOption.setOnClickListener { goingToSignInActivity() }
+
+
+    }
+
+    private fun signedUpUser() {
+        val email = binding.emailEt.text.toString()
+        val password = binding.passET.text.toString()
+        val confirmPassword = binding.confirmPassEt.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+            if(password == confirmPassword){
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent = Intent(this,SignInActivity::class.java)
+                        startActivity(intent)
+                        Toast.makeText(this,"Signed Up Successfully!",Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
+        else{
+            Toast.makeText(this,"Empty Fields are not Accepted",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun goingToSignInActivity() {
+        val intent = Intent(this,SignInActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/notes/UI/Activity/SignInActivity.kt
+++ app/src/main/java/com/example/notes/UI/Activity/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.example.notes.UI.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.notes.R
 import com.example.notes.databinding.ActivitySignInBinding
 import com.example.notes.databinding.ItemBinding
@@ -18,9 +19,42 @@ class SignInActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
 
         binding.textView.setOnClickListener {
-            val intent = Intent(this,SignInActivity::class.java)
+            val intent = Intent(this,SIgnUPActivity::class.java)
             startActivity(intent)
         }
 
+        binding.btnSignIn.setOnClickListener {
+            signedIn()
+        }
+
+    }
+
+    private fun signedIn() {
+        val email = binding.emailEt.text.toString()
+        val password = binding.passET.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if(it.isSuccessful){
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this,"Signed In Successfully!",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else{
+            Toast.makeText(this,"Empty Fields are not Accepted",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
